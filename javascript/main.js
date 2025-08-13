ecb126b42f705bfeee9e329db3ca520a1d341329
RAKOTOMIHAMINA Sanda Fitia <rakotomihaminasandafitia@gmail.com>
2025-08-13 14:14:38 +0300

    Create main.js

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+const cards = document.querySelectorAll('.card');
+let positions = ['active', 'right', 'left'];
+
+function updateClasses() {
+    cards.forEach((card, index) => {
+        card.className = 'card ' + positions[index];
+    });
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+    positions.unshift(positions.pop());
+    updateClasses();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    positions.push(positions.shift());
+    updateClasses();
+});
+
+updateClasses();
+
+document.getElementById("telecharger").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "doc/cv.pdf"; 
+    link.download = "cv_de_Sanda.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
