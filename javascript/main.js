13e90d876f3a2a20276c969cbf7de8cc9739dbea
RAKOTOMIHAMINA Sanda Fitia <rakotomihaminasandafitia@gmail.com>
2025-08-15 12:35:48 +0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,21 @@
 const cards = document.querySelectorAll('.card');
 let positions = ['active', 'right', 'left'];
+const message = "Etudiant de deuxième année en informatique général à l’Ecole Nationale d’Informatique, Université de Fianarantsoa. Je suis un jeune développeur passionné par les nouvelles technologies, notamment la programmation web. Je suis actuellement à la recherche d’un stage de trois mois pour acquérir de nouvelles expériences dans le monde professionnel et par la même occasion conclure mes études en deuxième année de licence.";
+let index = 0;
+const vitesse = 50;
 
 function updateClasses() {
     cards.forEach((card, index) => {
         card.className = 'card ' + positions[index];
     });
+}
+
+function afficherTexte() {
+  if (index < message.length) {
+    document.getElementById("texte").textContent += message.charAt(index);
+    index++;
+    setTimeout(afficherTexte, vitesse);
+  }
 }
 
 document.querySelector('.next').addEventListener('click', () => {
@@ -17,8 +28,6 @@ document.querySelector('.prev').addEventListener('click', () => {
     updateClasses();
 });
 
-updateClasses();
-
 document.getElementById("telecharger").addEventListener("click", function() {
     const link = document.createElement("a");
     link.href = "cv.pdf"; 
@@ -27,3 +36,6 @@ document.getElementById("telecharger").addEventListener("click", function() {
     link.click();
     document.body.removeChild(link);
 });
+
+updateClasses();
+afficherTexte();
